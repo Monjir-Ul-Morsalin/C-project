aaf09eaa031fa38486cd1b0ebee16ac4a7a0b18e
Khandaker Monjir-Ul-Morsalin <monjirulshaibal@gmail.com>
2019-06-28 12:43:05 +0600

    Initial commit

---
+++ Calculator .c
@@ -0,0 +1,58 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main()
+ {
+
+char operator;
+
+double  firstNumber,secondNumber;
+
+
+printf("Enter an operator (+, -, *,): ");
+
+scanf("%c", &operator);
+
+
+printf("Enter two operands: ");
+
+scanf("%lf %lf",&firstNumber, &secondNumber);
+
+
+switch(operator)
+
+    {
+
+case '+':
+
+printf("%.1lf + %.1lf = %.1lf",firstNumber, secondNumber, firstNumber + secondNumber);
+
+break;
+
+
+
+case '-':
+
+printf("%.1lf - %.1lf = %.1lf",firstNumber, secondNumber, firstNumber - secondNumber);
+
+break;
+
+
+
+case '*':
+
+printf("%.1lf * %.1lf = %.1lf",firstNumber, secondNumber, firstNumber * secondNumber);
+
+break;
+case '/':
+
+printf("%.1lf / %.1lf = %.1lf",firstNumber, secondNumber, firstNumber / secondNumber);
+
+break;
+
+default:
+
+printf("Error! operator is not correct");
+    }
+
+return 0;}
